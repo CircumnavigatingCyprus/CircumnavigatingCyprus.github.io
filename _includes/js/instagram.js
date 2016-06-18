c840bc66a35f69ad1098560eda23c03f657874c6
Ori Pleban <opleban@gmail.com>
2016-06-18 14:46:19 -0400

    Adds client id

--- _includes/js/instagram.js
+++ _includes/js/instagram.js
@@ -4,7 +4,7 @@ $(function($) {
   var feed = new Instafeed({
     get: 'tagged',
     tagName: 'circumnavigatingCY',
-    userId: '3052314434.1677ed0',
+    clientId: '05613cb4e3f34e36b44e826c09edb51f',
     accessToken: '3052314434.1677ed0.1609f26296c34ccca0d38cb563b986cc',
     sortyBy: 'most-recent',
     template: '<div class="item"><a href="{{link}}"><img src="{{image}}" /></a></div>',
